792f7b791f888eddee719d9cd06d1dd468b12a2c
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-31 10:17:41 +0200

    finished exercise

--- js/script.js
+++ js/script.js
@@ -5,14 +5,22 @@ console.log("JS OK");
 const myList = document.getElementById("myList");
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
 
+// contenuto base
 let content = i;
 
+// Considerazione dei multipli
 if (i % 15 === 0) content = "fizzbuzz"
 else if (i % 3 === 0) content = "fizz"
 else if (i % 5 === 0) content = "buzz"
 
-    console.log(content)
-    myList.innerHTML += `<li class="${content}>${content}</li>`
+
+
+// Preparo l'elemento LI
+const listItem = document.createElement ("li")
+listItem.append(content)
+if (isNaN(content)) listItem.className = content.toLowerCase();
+
+// Aggancio all'ul
+myList.appendChild(listItem)
 }
